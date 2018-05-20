8c8f3a3891a43e1c29ddd42318e4df9513b6e42d
Phil S <philipstubbs13@gmail.com>
2018-05-20 00:09:24 -0500

    authentication working on server side

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
 const express = require('express');
-const app = express();
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 
-// Route handler
-app.get('/', (req, res) => {
-    res.send({ bye: 'buddy' })
-});
+const app = express();
+ 
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback'
+    }, 
+    (accessToken, refreshToken, profile, done) => {
+        console.log('access token', accessToken);
+        console.log('refresh token', refreshToken);
+        console.log('profile: ', profile);
+    }
+  )
+);
+
+// Route handlers
+app.get(
+  '/auth/google', 
+  passport.authenticate('google', {
+    scope: ['profile', 'email']
+  })
+);
+
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
