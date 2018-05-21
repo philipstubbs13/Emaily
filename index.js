3203422d669fd6309e35b30c7263e257efbfc138
Phil S <philipstubbs13@gmail.com>
2018-05-20 21:37:18 -0500

    enabling cookies

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const keys = require('./config/keys');
 require('./models/User');
 require('./services/passport');
@@ -7,6 +9,18 @@ require('./services/passport');
 mongoose.connect(keys.mongoURI);
 
 const app = express();
+
+// Tell express to make use of cookies inside application
+app.use(
+  cookieSession({
+      maxAge: 30 * 24 * 60 * 60 * 1000,
+      keys: [keys.cookieKey]
+  })
+);
+
+// Tell passport to make use of cookies to handle authentication
+app.use(passport.initialize());
+app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 
