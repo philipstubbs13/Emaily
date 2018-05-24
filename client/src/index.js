f7ee8cb3612e82183e0113b16cd41e2d9233575b
Phil S <philipstubbs13@gmail.com>
2018-05-23 20:13:31 -0500

    root module install

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import reactDOM from 'react-dom';
+import App from './components/App';
 
-reactDOM.render();
+reactDOM.render(<App />, document.querySelector('#root'));
