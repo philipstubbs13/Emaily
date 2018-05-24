30f841c6f1ed26442cd2e769af2324b4cab2dc73
Phil S <philipstubbs13@gmail.com>
2018-05-23 19:26:54 -0500

    refactored passport.js with ES2017 async/await syntax

--- services/passport.js
+++ services/passport.js
@@ -23,19 +23,16 @@ passport.use(
             callbackURL: '/auth/google/callback',
             proxy: true
         },
-        (accessToken, refreshToken, profile, done) => {
-          User.findOne({ googleId: profile.id })
-            .then((existingUser) => {
-              if (existingUser) {
-                // We alread have a reacord with the given profile ID.
-                done(null, existingUser);
-              }
-              else {
-                // We don't have a user record with this ID, make a new record.
-                new User({ googleId: profile.id }).save()
-                  .then(user => done(null, user));
-              }
-            })  
+        async (accessToken, refreshToken, profile, done) => {
+          const existingUser = await User.findOne({ googleId: profile.id })
+            
+            if (existingUser) {
+              // We alread have a reacord with the given profile ID.
+              return done(null, existingUser);
+            }
+            // We don't have a user record with this ID, make a new record.
+            const user = await new User({ googleId: profile.id }).save()
+            done(null, user);
         }
     )
 );
