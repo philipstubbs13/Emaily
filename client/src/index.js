2a6dff160f413a22a88cf5126441a822d3185b00
Phil S <philipstubbs13@gmail.com>
2018-05-29 06:51:42 -0500

    react env variables

--- client/src/index.js
+++ client/src/index.js
@@ -18,3 +18,6 @@ reactDOM.render(
 <Provider store={store}><App /></Provider>,
  document.querySelector('#root')
 );
+
+console.log('STRIPE KEY IS', process.env.REACT_APP_STRIPE_KEY);
+console.log('Environment is', process.env.NODE_ENV);
