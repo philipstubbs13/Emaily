64a6bd38c8c6c28ae9eba6573821f75269d2ffda
Phil S <philipstubbs13@gmail.com>
2018-06-05 04:30:19 -0500

    email sent successfully!

--- services/Mailer.js
+++ services/Mailer.js
@@ -6,6 +6,7 @@ class Mailer extends helper.Mail {
   constructor({ subject, recipients }, content) {
     super();
 
+    this.sgApi = sendgrid(keys.sendGridKey);
     this.from_email = new helper.Email('no-reply@emaily.com');
     this.subject = subject;
     this.body = new helper.Content('text/html', content);
@@ -16,21 +17,48 @@ class Mailer extends helper.Mail {
     this.addRecipients();
   }
 
-    formatAddresses(recipients) {
-        return recipients.map(({ email }) => {
-            return new helper.Email(email);
-        });
-    }
+  formatAddresses(recipients) {
+    return recipients.map(({ email }) => {
+      return new helper.Email(email);
+    });
+  }
 
-    addClickTracking() {
-       const trackingsSettings = new helper.TrackingSettings();
-       const clickTracking = new helper.ClickTracking(true, true); 
+  addClickTracking() {
+    const trackingSettings = new helper.TrackingSettings();
+    const clickTracking = new helper.ClickTracking(true, true); 
 
-       trackingSettings.setClickTracking(clickTracking);
-       this.addTrackingSettings(trackingSettings);
-    }
+    trackingSettings.setClickTracking(clickTracking);
+    this.addTrackingSettings(trackingSettings);
+  }
+
+  addRecipients() {
+    const personalize = new helper.Personalization();
+    this.recipients.forEach(recipient => {
+        personalize.addTo(recipient);
+    });
+    this.addPersonalization(personalize);
+  }
+  
+  async send() {
+    const request = this.sgApi.emptyRequest({
+      method: 'POST',
+      path: '/v3/mail/send',
+      body: this.toJSON()
+    });
+
+    const response = this.sgApi.API(request);
+    return response;
+  }
 }
 
-
-
 module.exports = Mailer;
+
+//Testing email sending
+// const survey = {
+//     title: 'my title',
+//     subject: 'my subject',
+//     recipients: 'philipstubbs13@gmail.com',
+//     body: 'heres the body of the email'
+// };
+// survey
+//axios.post('/api/surveys', survey);
