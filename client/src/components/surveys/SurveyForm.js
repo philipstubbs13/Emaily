976c331daf76622dcfe03afd1f4fd6f423834649
Phil S <philipstubbs13@gmail.com>
2018-06-07 06:18:22 -0500

    field validation

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -6,10 +6,10 @@ import { Link } from 'react-router-dom';
 import SurveyField from './SurveyField';
 
 const FIELDS = [
-    { label: 'Survey Title', name: 'title' },
-    { label: 'Subject Line', name: 'subject' },
-    { label: 'Email Body', name: 'body' },
-    { label: 'Recipient List', name: 'emails' }
+    { label: 'Survey Title', name: 'title', noValueError: 'You must provide a title.' },
+    { label: 'Subject Line', name: 'subject', noValueError: 'You must provide a subject.' },
+    { label: 'Email Body', name: 'body', noValueError: 'You must provide an email body.' },
+    { label: 'Recipient List', name: 'emails', noValueError: 'You must provide a list of recipients.' }
 ];
 
 class SurveyForm extends Component {
@@ -35,9 +35,12 @@ class SurveyForm extends Component {
 function validate(values) {
     const errors = {};
 
-    if (!values.title) {
-        errors.title= 'You must provide a title';
-    }
+    _.each(FIELDS, ({ name, noValueError }) => {
+        if (!values[name]) {
+            errors[name] = noValueError;
+        }
+    });
+    
     return errors;
 }
 
