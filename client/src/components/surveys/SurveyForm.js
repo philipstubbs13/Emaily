0c33de518374604c85e43943ca83ce5a0855aeff
Phil S <philipstubbs13@gmail.com>
2018-06-08 19:42:33 -0500

    form fields

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -5,17 +5,11 @@ import { reduxForm, Field } from 'redux-form';
 import { Link } from 'react-router-dom';
 import SurveyField from './SurveyField';
 import validateEmails from '../../utils/validateEmails';
-
-const FIELDS = [
-    { label: 'Survey Title', name: 'title', noValueError: 'You must provide a title.' },
-    { label: 'Subject Line', name: 'subject', noValueError: 'You must provide a subject.' },
-    { label: 'Email Body', name: 'body', noValueError: 'You must provide an email body.' },
-    { label: 'Recipient List', name: 'emails', noValueError: 'You must provide a list of recipients.' }
-];
+import formFields from './formFields';
 
 class SurveyForm extends Component {
     renderFields() {
-        return _.map(FIELDS, ({ label, name }) => {
+        return _.map(formFields, ({ label, name }) => {
             return <Field key={name} component={SurveyField} type="text" label={label} name={name} />
         });
     }
@@ -38,7 +32,7 @@ function validate(values) {
 
     errors.emails = validateEmails(values.emails || '');
 
-    _.each(FIELDS, ({ name, noValueError }) => {
+    _.each(formFields, ({ name, noValueError }) => {
         if (!values[name]) {
             errors[name] = noValueError;
         }
